561600de31b8c05bd33f55c650d8444dbd0445a5
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-12-18 19:56:06 +0530

    provider added

---
+++ components/Providers.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+import { WagmiProvider } from 'wagmi';
+
+import {
+  mantleTestnet,
+} from 'wagmi/chains';
+
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+import { ReactNode } from 'react';
+
+const config = getDefaultConfig({
+  appName: 'x402',
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'YOUR_PROJECT_ID',
+  chains: [mantleTestnet],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
